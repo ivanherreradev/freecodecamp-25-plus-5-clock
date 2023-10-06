e5869388be8419266faa5b5948ec2eb888493857
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-06 09:49:55 -0300

    add play state and function to control timeout

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,11 @@ import { timeFormatter } from './utils/helpers/timeFormatter'
 import './App.css'
 
 const App = () => {
-  const [timingType, setTimingtype] = useState('SESSION')
+  const [timingType, setTimingType] = useState('SESSION')
   const [timeLeft, setTimeLeft] = useState(60 * 25)
   const [breakLength, setBreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
+  const [play, setPlay] = useState(false)
 
   const incrementBreak = () => {
     if (breakLength < 60) {
@@ -38,6 +39,17 @@ const App = () => {
     }
   }
 
+  const timeout = setTimeout(() => {
+    if (timeLeft && play) {
+      setTimeLeft(timeLeft - 1)
+    }
+  }, 1000)
+
+  const handlePlay = () => {
+    clearTimeout(timeout)
+    setPlay(!play)
+  }
+
   const formattedTime = timeFormatter(timeLeft)
   return (
     <main>
@@ -48,7 +60,9 @@ const App = () => {
       />
       <article>
         <Timer title={timingType} formattedTime={formattedTime} />
-        <Button id="start_stop">Stop/Start</Button>
+        <Button id="start_stop" onClick={handlePlay}>
+          {play ? 'Stop' : 'Start'}
+        </Button>
         <Button id="reset">Reset</Button>
       </article>
       <Session
