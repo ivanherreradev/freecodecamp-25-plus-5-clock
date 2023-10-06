5feca776af6fe98ecbe27e6bc52654dec327a521
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-06 09:22:52 -0300

    add timeFormatted helper

---
+++ src/utils/helpers/timeFormatter.js
@@ -0,0 +1,7 @@
+export const timeFormatter = (time) => {
+  const minutes = Math.floor(time / 60)
+  const seconds = time - minutes * 60
+  const formattedSeconds = seconds < 10 ? '0' + seconds : seconds
+  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes
+  return `${formattedMinutes}:${formattedSeconds}`
+}
